42c65077f1ee8757b12ce533c125c78a14f3cdd4
amyhean <72956190+Amyhean@users.noreply.github.com>
2026-08-20 13:06:34 -0600

    Corrige rutas footer

--- JS/footer.js
+++ JS/footer.js
@@ -1,35 +1,91 @@
 let footer = document.getElementById("footer");
+
 footer.insertAdjacentHTML("beforebegin",
-        `<footer class="footer-clean">
-          <div class="container">
-              <div class="row justify-content-center">
-                  <div class="col-sm-3 col-md-2 item">
-                    <a href="mailto:info@parquejoquicingo.org" target="_blank" style="margin-right:20px;"><img src="./SRC/img/sobre.png" alt="" width="35px" height="35px"></a>
-                    <a href="https://www.facebook.com/Joxicingo/" target="_blank" style="margin-right:20px;"><img src="./SRC/img/facebook.png" alt="" width="35px" height="35px"></a>
-                    <a href="https://www.instagram.com/" target="_blank"><img src="./SRC/img/instagram.png" alt="" width="35px" height="35px"></a>
-                  </div>
-                   <div class="col-sm-3 col-md-2 item">
-                      <h3>Invitanos un cafe</h3>
-                      <ul>
-                        <li>
-                          <a href="../otros.html">Otros parques en México</a>
-                        </li>
-                        <li>
-                          <a href="../otros.html">Parques en otros Paises</a>
-                        </li>
-                      </ul>
-                  </div>
-                  <div class="col-sm-3 col-md-2 item">
-                      <h3>Actividades</h3>
-                      <ul>
-                          <li><a href="../aproximas.html" target="_blank">Proximas</a></li>
-                          <li><a href="../actividadesr.html">Anteriores</a></li>
-                      </ul>
-                  </div>
-                  <div class="col-lg-3 item social">
-                    <img src="./src/img/2.png" alt="logo" width="300px" height="200px">
-                  </div>
-                  <h5 class="copyright">Parque de estudios y reflexión Joquicingo © 2024</h5>
-              </div>
-          </div>
-      </footer>`);
+`<footer class="footer-clean">
+    <div class="container">
+        <div class="row justify-content-center">
+
+            <div class="col-sm-3 col-md-2 item">
+
+                <a href="mailto:info@parquejoquicingo.org"
+                   target="_blank"
+                   style="margin-right:20px;">
+                    <img src="./SRC/img/sobre.png"
+                         alt=""
+                         width="35px"
+                         height="35px">
+                </a>
+
+                <a href="https://www.facebook.com/Joxicingo/"
+                   target="_blank"
+                   style="margin-right:20px;">
+                    <img src="./SRC/img/facebook.png"
+                         alt=""
+                         width="35px"
+                         height="35px">
+                </a>
+
+                <a href="https://www.instagram.com/"
+                   target="_blank">
+                    <img src="./SRC/img/instagram.png"
+                         alt=""
+                         width="35px"
+                         height="35px">
+                </a>
+
+            </div>
+
+            <div class="col-sm-3 col-md-2 item">
+                <h3>Invitanos un cafe</h3>
+
+                <ul>
+                    <li>
+                        <a href="./otros.html">
+                            Otros parques en México
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="./otros.html">
+                            Parques en otros Paises
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-sm-3 col-md-2 item">
+
+                <h3>Actividades</h3>
+
+                <ul>
+                    <li>
+                        <a href="./aproximas.html">
+                            Proximas
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="./actividadesr.html">
+                            Anteriores
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+
+            <div class="col-lg-3 item social">
+
+                <img src="./SRC/img/2.png"
+                     alt="logo"
+                     width="300px"
+                     height="200px">
+
+            </div>
+
+            <h5 class="copyright">
+                Parque de estudios y reflexión Joquicingo © 2024
+            </h5>
+
+        </div>
+    </div>
+</footer>`);
